21b2a938449a01f7ba342c4cd991cc91581ea96f
brendan360 <67534108+brendan360@users.noreply.github.com>
2024-04-27 18:57:05 +1000

    Create gauge.py
    beginnning

---
+++ gauge.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+
+######
+# SET UPS
+######
+from __future__ import absolute_import, division, print_function, \
+                                                    unicode_literals
+import time
+import os
+
+try:
+    from ADCPi import ADCPi
+except ImportError:
+    print("Failed to import ADCPi from python system path")
+    print("Importing from parent folder instead")
+    try:
+        import sys
+        sys.path.append('..')
+        from ADCPi import ADCPi
+    except ImportError:
+        raise ImportError(
+            "Failed to import library from parent folder")
+
+
+adc = ADCPi(0x68, 0x69, 12)
+
+
+######
+# VARIABLES
+######
+
+
+
+######
+#
+######
+
+#reading voltages     adc.read_voltage(1)
+
+
+######
+#
+######
+
+
+
+
+
+######
+#
+######
+
