ea312c92d38f2e20cabf02ccb0b8d0620184be05
brendan360 <67534108+brendan360@users.noreply.github.com>
2024-05-04 23:19:02 +1000

    Update gauge.py

--- gauge.py
+++ gauge.py
@@ -66,4 +66,4 @@ for key in gaugeItems.keys():
     print(adc.read_voltage(int(gaugeItems[key][0])))
     gaugeItems[key][2]=adc.read_voltage(int(gaugeItems[key][0]))
 
-print (gaugeItems["FUEL_PRESSURE"[2])
+print (gaugeItems["FUEL_PRESSURE"][2])
