1348bf5dee941fbedcffe052e4ac31525a0ba6e0
brendan360 <67534108+brendan360@users.noreply.github.com>
2024-05-05 12:07:08 +1000

    Update gauge.py

--- gauge.py
+++ gauge.py
@@ -55,6 +55,7 @@ gaugeItems={
 ######
 def FUNCT_fuel_pres():
     voltage=adc.read_voltage(int(gaugeItems["FUEL_PRESSURE"][0]))
+    print(voltage)
     voltage_normalised= voltage/1023.0 *4.5
     print(voltage_normalised)
     gaugeItems["FUEL_PRESSURE"][2]= (voltage_normalised - .5)/(4.5 -.5)*(1000- 0) + 0
