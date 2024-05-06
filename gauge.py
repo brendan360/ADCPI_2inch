700817a30681d8013f7c7aa79760f3e9d75cd37f
brendan360 <67534108+brendan360@users.noreply.github.com>
2024-05-06 23:19:44 +1000

    Update gauge.py

--- gauge.py
+++ gauge.py
@@ -113,7 +113,7 @@ def FUNCT_boost_pres():
 
 def FUNCT_block_temp():
     voltage=adc.read_voltage(int(gaugeItems["BLOCK_TEMP"][0]))
-print(voltage)
+    print(adc.read_voltage(int(gaugeItems["BLOCK_TEMP"][0])))
     Vr2 = voltage
     R2=Vr2*10000/(5-Vr2)
     steinhart = R2 / CONST_blockTempresistorRoomTemp
@@ -122,7 +122,7 @@ print(voltage)
     steinhart += 1 / (29.315 + 273.15)
     steinhart = 1 / steinhart
     steinhart -= 273.15
-print(steinhart)
+    print(steinhart)
 
 ######
 # MAIN
