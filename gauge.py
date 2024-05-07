a054908b0753eb2638843b5dc8d804d84e91fb1a
brendan360 <67534108+brendan360@users.noreply.github.com>
2024-05-07 23:23:41 +1000

    Update gauge.py

--- gauge.py
+++ gauge.py
@@ -108,7 +108,7 @@ def FUNCT_oil_pres():
 def FUNCT_boost_pres():
     voltage=adc.read_voltage(int(gaugeItems["BOOST"][0]))
     boostKpa= (voltage - CONST_boost_minVoltage)/(CONST_boost_maxVoltage -CONST_boost_minVoltage)*(CONST_boost_maxPressure- CONST_boost_minPressure) + CONST_boost_minPressure
-    gaugeItems["BOOST"][2]=((boostKpa-98.3)*0.145038)
+    gaugeItems["BOOST"][2]=((boostKpa-94.3)*0.145038)
     if gaugeItems["BOOST"][2] < 0:
         gaugeItems["BOOST"][9] = "inHg"
         gaugeItems["BOOST"][2]=(abs(gaugeItems["BOOST"][2])*2.03602)
