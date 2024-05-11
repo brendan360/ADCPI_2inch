c76849df34fce6e15ee2871bf67c26a88347e452
brendan360 <67534108+brendan360@users.noreply.github.com>
2024-05-11 10:34:57 +1000

    Update gauge.py

--- gauge.py
+++ gauge.py
@@ -37,14 +37,14 @@ adc = ADCPi(0x68, 0x69, 12)
  
 gaugeItems={
 #   NAME,          value, display name warninglow,alertlow,warninghigh,alerthigh,rangelow,rangehigh,measurment,alertcount 
-  "FUEL_PRESSURE":["1","Fuel Pres.", 0, 10,15,99,110,0,150,"Kpa", 0],               #
-  "BOOST":["2","Boost.", 0, 10,15,99,110,0,150,"psi", 0],                       #
-  "BLOCK_TEMP":["3","Engine °C ", 0, 10,15,99,110,0,150,"°C", 0],
-  "COOLANT_PRESSURE":["4","H2O Pres.", 0, 10,15,99,110,0,150,"Kpa", 0],            #
-  "COOLANT_TEMP":["5","H2O °C", 0, 10,15,99,110,0,150,"°C", 0],
-  "OIL_PRESSURE":["6","Oil Pres.", 0, 10,15,99,110,0,150,"Kpa", 0],                #
-  "OIL_TEMP":["7","Oil °C", 0, 10,15,99,110,0,150,"°C", 0],
-  "WIDEBAND02":["8","O2 AFR", 0, 10,15,99,110,0,150,"A/F", 0]
+  "FUEL_PRESSURE":["1","Fuel Pres.", 1, 10,15,99,110,0,150,"Kpa", 0],               #
+  "BOOST":["2","Boost.", 1, 10,15,99,110,0,150,"psi", 0],                       #
+  "BLOCK_TEMP":["3","Engine °C ", 1, 10,15,99,110,0,150,"°C", 0],
+  "COOLANT_PRESSURE":["4","H2O Pres.", 1, 10,15,99,110,0,150,"Kpa", 0],            #
+  "COOLANT_TEMP":["5","H2O °C", 1, 10,15,99,110,0,150,"°C", 0],
+  "OIL_PRESSURE":["6","Oil Pres.", 1, 10,15,99,110,0,150,"Kpa", 0],                #
+  "OIL_TEMP":["7","Oil °C", 1, 10,15,99,110,0,150,"°C", 0],
+  "WIDEBAND02":["8","O2 AFR", 1, 10,15,99,110,0,150,"A/F", 0]
 }
 
 ######
