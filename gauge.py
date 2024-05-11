2cc85428c289257d22c192506403150876b4d9ff
brendan360 <67534108+brendan360@users.noreply.github.com>
2024-05-11 11:04:08 +1000

    Update gauge.py

--- gauge.py
+++ gauge.py
@@ -38,7 +38,7 @@ adc = ADCPi(0x68, 0x69, 12)
 gaugeItems={
 #   NAME,          value, display name warninglow,alertlow,warninghigh,alerthigh,rangelow,rangehigh,measurment,alertcount 
   "FUEL_PRESSURE":["1","Fuel Pres.", 1, 10,15,99,110,0,150,"Kpa", 0],               #
-  "BOOST":["2","Boost.", 1, 10,15,99,110,0,150,"psi", 0],                       #
+  "BOOST":["2","Boost", 1, 10,15,99,110,0,150,"psi", 0],                       #
   "BLOCK_TEMP":["3","Engine °C ", 1, 10,15,99,110,0,150,"°C", 0],
   "COOLANT_PRESSURE":["4","H2O Pres.", 1, 10,15,99,110,0,150,"Kpa", 0],            #
   "COOLANT_TEMP":["5","H2O °C", 1, 10,15,99,110,0,150,"°C", 0],
